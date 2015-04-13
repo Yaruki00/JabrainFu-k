131a1ef31f79a0dd8a0b9ab72ef0f5716bdb0de3
Yuta Kawabe <yuyu3165@gmail.com>
2015-04-13 23:28:48 +0900

    fix indent and comment

--- BF_interpreter.java
+++ BF_interpreter.java
@@ -6,71 +6,71 @@ public class BF_interpreter {
 
     public static void main(String[] args) throws Exception {
 
-    // memory
-    byte buff[] = new byte[30000];
-    Arrays.fill(buff, (byte) 0);
+	// memory
+	byte buff[] = new byte[30000];
+	Arrays.fill(buff, (byte) 0);
 
-    // code
-    String code_str = "+++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-.------------.<++++++++.--------.+++.------.--------.>+.";
-    char code[] = code_str.toCharArray();
+	// code
+	String code_str = "+++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-.------------.<++++++++.--------.+++.------.--------.>+.";
+	char code[] = code_str.toCharArray();
     
-    // pointer and loop count
-    int code_ptr = 0, buff_ptr = 0;
+	// pointer
+	int code_ptr = 0, buff_ptr = 0;
     
-    // IO
-    InputStreamReader consoleReader = new InputStreamReader(System.in);
-    OutputStream outWriter = System.out;
+	// IO
+	InputStreamReader consoleReader = new InputStreamReader(System.in);
+	OutputStream outWriter = System.out;
     
-    // interpreter
-    for (; code_ptr < code.length; code_ptr++) {
-	switch (code[code_ptr]) {
-	case '>':
-	    buff_ptr++;
-	    break;
-	case '<':
-	    buff_ptr--;
-	    break;
-	case '+':
-	    buff[buff_ptr]++;
-	    break;
-	case '-':
-	    buff[buff_ptr]--;
-	    break;
-	case '.':
-	    outWriter.write((char) buff[buff_ptr]);
-	    outWriter.flush();
-	    break;
-	case ',':
-	    buff[buff_ptr] = (byte) consoleReader.read();
-	    break;
-	case '[':
-	    if (buff[buff_ptr] == 0) {
-		code_ptr++;
-		for (int loop_cnt = 1; loop_cnt > 0; code_ptr++) {
-		    if (code[code_ptr] == '[') {
-			loop_cnt++;
-		    } else if (code[code_ptr] == ']') {
-			loop_cnt--;
+	// interpreter
+	for (; code_ptr < code.length; code_ptr++) {
+	    switch (code[code_ptr]) {
+	    case '>':
+		buff_ptr++;
+		break;
+	    case '<':
+		buff_ptr--;
+		break;
+	    case '+':
+		buff[buff_ptr]++;
+		break;
+	    case '-':
+		buff[buff_ptr]--;
+		break;
+	    case '.':
+		outWriter.write((char) buff[buff_ptr]);
+		outWriter.flush();
+		break;
+	    case ',':
+		buff[buff_ptr] = (byte) consoleReader.read();
+		break;
+	    case '[':
+		if (buff[buff_ptr] == 0) {
+		    code_ptr++;
+		    for (int loop_cnt = 1; loop_cnt > 0; code_ptr++) {
+			if (code[code_ptr] == '[') {
+			    loop_cnt++;
+			} else if (code[code_ptr] == ']') {
+			    loop_cnt--;
+			}
 		    }
 		}
-	    }
-	    break;
-	case ']':
-	    if (buff[buff_ptr] != 0) {
-		code_ptr--;
-		for (int loop_cnt = 1; loop_cnt > 0; code_ptr--) {
-		    if (code[code_ptr] == '[') {
-			loop_cnt--;
-		    } else if (code[code_ptr] == ']') {
-			loop_cnt++;
+		break;
+	    case ']':
+		if (buff[buff_ptr] != 0) {
+		    code_ptr--;
+		    for (int loop_cnt = 1; loop_cnt > 0; code_ptr--) {
+			if (code[code_ptr] == '[') {
+			    loop_cnt--;
+			} else if (code[code_ptr] == ']') {
+			    loop_cnt++;
+			}
 		    }
+		    code_ptr++;
 		}
-		code_ptr++;
+		break;
 	    }
-	    break;
+
 	}
-
-    }
 
     }
 
